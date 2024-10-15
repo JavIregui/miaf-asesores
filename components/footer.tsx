12f81fb335c788df5832dbbaa697dce11f986984
Javier Iregui <iregui.javi@gmail.com>
2024-10-15 22:34:20 +0200

    Footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,28 +2,28 @@ import Image from 'next/image';
 
 export const Footer = () => {
     return (
-        <footer className="w-full bg-miaf-gray-100 pt-12 pb-16 px-12 md:px-20 lg:px-32 xl:px-48 2xl:px-56">
-            <div className='flex space-x-12'>
-            <a href="/" className="flex items-center">
-                <div className="relative w-32 h-16">
-                    <Image
-                        fill
-                        alt="Logo MIAF Asesores"
-                        src="/logoDark.png"
-                        priority
-                    />
-                </div>
-            </a>
-            <a href="/" className="flex items-center">
-                <div className="relative w-48 h-24">
-                    <Image
-                        fill
-                        alt="Logo MIAF Asesores"
-                        src="/uniLogo.png"
-                        priority
-                    />
-                </div>
-            </a>
+        <footer className="w-full bg-miaf-gray-100 pt-12 pb-16 flex flex-col space-y-4 items-center px-12 sm:items-start md:px-20 lg:px-32 xl:px-48 2xl:px-56">
+            <div className='flex space-x-8 sm:space-x-12'>
+                <a href="/" className="flex items-center">
+                    <div className="relative w-32 h-16">
+                        <Image
+                            fill
+                            alt="Logo MIAF Asesores"
+                            src="/logoDark.png"
+                            priority
+                        />
+                    </div>
+                </a>
+                <a href="https://isde.es" className="flex items-center">
+                    <div className="relative w-48 h-24">
+                        <Image
+                            fill
+                            alt="Logo MIAF Asesores"
+                            src="/uniLogo.png"
+                            priority
+                        />
+                    </div>
+                </a>
             </div>
         </footer>
     );
