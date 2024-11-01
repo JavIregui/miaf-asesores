00f00445b794e47676666ffdb5507eae29231559
Javier Iregui <iregui.javi@gmail.com>
2024-11-01 08:02:40 +0100

    Dashboard navbar

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,6 +1,43 @@
+"use client"
+
+import { Button } from '@/components/ui/button';
+import { client } from '@/lib/pocketbase';
+import { LogOut } from 'lucide-react';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
 export default function Admin() {
 
+    const [userName, setUserName] = useState('Usuario invitado');
+
+    useEffect(() => {
+        if (client.authStore.isValid) {
+            setUserName(client.authStore.model?.name || 'Usuario invitado');
+        }
+    }, []);
+
     return (
-        <h1>Dashboard</h1>
+        <>
+            <div className="flex justify-between items-center py-4 shadow-sm px-12 md:px-10 lg:px-16 xl:px-24 2xl:px-28">
+                <div className='flex space-x-8 items-center'>
+                    <Image
+                        alt="Logo MIAF Asesores"
+                        src="/img/logoDark.png"
+                        width={96}
+                        height={40}
+                        priority
+                    />
+                    <h1 className='invisible sm:visible font-roboto font-normal text-miaf-gray-300 text-xl'>
+                        Bienvenido <span className='font-bold'>{userName}</span>
+                    </h1>
+                </div>
+                
+                <Button variant="miaf" size="miafSmall">
+                    Cerrar sesión
+                    <LogOut className="h-4 w-4" />
+                </Button>
+            </div>
+        </>
+        
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="theme-color" content="#242C53" />
       </head>
