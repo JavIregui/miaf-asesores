d9e2a76035019a61320f85ebc1cb083200357614
Javier Iregui <iregui.javi@gmail.com>
2024-11-01 08:18:03 +0100

    fix?

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,6 +4,7 @@ export const Footer = () => {
     return (
         <footer className="w-full bg-miaf-gray-100 pt-12 pb-16 flex flex-col space-y-4 px-12 sm:space-y-2 sm:items-start md:px-20 lg:px-32 xl:px-48 2xl:px-56">
             <div className='flex space-x-8 sm:space-x-12'>
+                {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
                 <a href="/" className="flex items-center">
                     <div className="relative w-24 h-11 sm:w-32 sm:h-16">
                         <Image
@@ -14,6 +15,7 @@ export const Footer = () => {
                         />
                     </div>
                 </a>
+                {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
                 <a href="https://isde.es" className="flex items-center">
                     <div className="relative w-28 h-14 sm:w-48 sm:h-24">
                         <Image
